0b1074fa22da464de37b51e101fc8da1a42cb8b9
SunnyLy <450512747@qq.com>
2020-05-24 16:21:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/ErrController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrController {
+   /* @RequestMapping("/error")
+    public String error() {
+        return "对不起，网页不见了！";
+    }*/
+}
